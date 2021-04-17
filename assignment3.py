ddbe98f811f181281f58511565056ede7c256e6a
lauravik <74673514+lauravik@users.noreply.github.com>
2021-04-17 17:08:18 +0200

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,4 +1,8 @@
-pip install influxdb
+import subprocess
+import sys
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+install(influxdb)
 from influxdb import InfluxDBClient 
 import pandas as pd
 from sklearn.pipeline import Pipeline
